535988d6d8d41f89279d55af6bb0d747670f9b17
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-25 17:58:26 -0300

    :sparkles: Feat: create products service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product, ProductDocument } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+  ) {}
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const product = new this.productModel(createProductDto);
+    return product.save();
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.productModel.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string) {
+    return this.productModel.findById(id);
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: string, updateProductDto: UpdateProductDto) {
+    return this.productModel.findByIdAndUpdate(
+      {
+        _id: id,
+      },
+      {
+        $set: updateProductDto,
+      },
+      {
+        new: true,
+      },
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  remove(id: string) {
+    return this.productModel
+      .remove({
+        _id: id,
+      })
+      .exec();
   }
 }
