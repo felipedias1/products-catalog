b72c0771ac31583e43094beb07665fb94f9996c1
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-25 17:56:56 -0300

    Import connection to mongoDB and productsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(`mongodb://localhost:27017/ProductsCatalog`),
+    ProductsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
