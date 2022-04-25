c775faa585bfc637202dba4f480d8c28d8727938
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-25 17:59:41 -0300

    :sparkles: Feat: create product class and schema

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,21 @@
-export class Product {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema()
+export class Product {
+  @Prop()
+  nome: string;
+
+  @Prop()
+  quantidade: number;
+
+  @Prop()
+  ativo: number;
+
+  @Prop()
+  categoria_id: number;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
