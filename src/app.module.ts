f95f568273e7833be474a437d3676252181aec19
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-25 22:18:53 -0300

    :sparkles: Feat: Add CategoriesModule of app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(`mongodb://localhost:27017/ProductsCatalog`),
     ProductsModule,
+    CategoriesModule,
   ],
   controllers: [],
   providers: [],
