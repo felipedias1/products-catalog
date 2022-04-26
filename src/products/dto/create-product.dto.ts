aacc1720b8c78a2ad205e1eb369fc1c5369b6860
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-26 11:37:48 -0300

    :sparkles: Feat: create validation type of product props

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,15 @@
+import { IsMongoId, IsNumber, IsString } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
   nome: string;
+
+  @IsNumber()
   quantidade: number;
+
+  @IsNumber()
   ativo: number;
+
+  @IsMongoId()
   categoria_id: string;
 }
