3d07d51ba6052e5c22c2f7f812e67d67b0b79560
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-27 00:40:20 -0300

    :sparkles: Feat: Create extra route to convert json to pdf categories

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -5,6 +5,7 @@ import { Category, CategoryDocument } from './entities/category.entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
+import jsPDF from 'jspdf';
 
 @Injectable()
 export class CategoriesService {
@@ -46,6 +47,28 @@ export class CategoriesService {
       );
     }
     return category;
+  }
+
+  async generatePdf() {
+    const categories = await this.categoryModel.find({});
+
+    const doc = new jsPDF();
+    categories.forEach(function (categories, i) {
+      doc.text(
+        '_id: ' +
+          categories._id +
+          '\n' +
+          ' nome: ' +
+          categories.nome +
+          '\n' +
+          ' ativo: ' +
+          categories.ativo +
+          '\n\n\n',
+        20,
+        20 + i * 30,
+      );
+    });
+    doc.save('categories.pdf');
   }
 
   async update(id: string, updateCategoryDto: UpdateCategoryDto) {
