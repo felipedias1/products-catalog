587d86cd46d5c3d4a3853c3b55f67bd803e38e59
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-27 15:32:21 -0300

    :sparkles: Feat: import User and Auth Modules to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(`mongodb://localhost:27017/ProductsCatalog`),
     ProductsModule,
     CategoriesModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
