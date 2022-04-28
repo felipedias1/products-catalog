fe34e0af617e73267d8feaa0d10a68b382b024b0
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-27 21:46:18 -0300

    :sparkles: Feat: Build User Service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+// This should be a real class/interface representing a user entity
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'adm',
+      password: 'adm123',
+    },
+    {
+      userId: 2,
+      username: 'teste',
+      password: 'teste123',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
