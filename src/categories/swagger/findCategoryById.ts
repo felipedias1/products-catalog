ec9917e36ccf3a37d85667aa1d89e1ae77627d23
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-28 13:00:52 -0300

    :sparkles: Feat: Create "classType" to Find Category by Id

---
+++ src/categories/swagger/findCategoryById.ts
@@ -0,0 +1,13 @@
+import { Prop } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Category } from '../entities/category.entity';
+
+export class FindCategorytById extends Category {
+  @Prop()
+  @ApiProperty()
+  _id: string;
+
+  @Prop()
+  @ApiProperty()
+  __v: number;
+}
